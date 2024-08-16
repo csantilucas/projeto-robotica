8b92e49c4eaf36456fa6435f0f3d63b05213979f
csantilucas <lucascsanti2006@gmail.com>
2024-08-16 00:38:33 -0400

    menu atualizado

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-var btn = document.querySelector("#btn");
-btn.addEventListener("click", function() {
-    var div = document.querySelector("#container");
-    
-  if(div.style.display === "none") {
-        div.style.display = "block";
-    } else {
-      div.style.display = "none";
-  }
-    
-});
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 50,
+    loop: true,
+    centerSlide: true,
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
